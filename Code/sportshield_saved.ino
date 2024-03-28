dceb1109411742652beacb10a3c0ea3d60a80f60
Wilfried Portet <wilfried.portet@algosup.com>
2024-03-28 13:45:11 +0100

    password modified

--- Code/sportshield_saved.ino
+++ Code/sportshield_saved.ino
@@ -525,7 +525,7 @@ void onDisconnect(BLEDevice central) {
   digitalWrite(LEDB, HIGH);
 }
 void onWritePassword(BLEDevice central, BLECharacteristic characteristic) {
-  const int motDePasseAttendu = 13330;
+  const int motDePasseAttendu = 1;
   short int value = PasswordCharacteristic.value();
   // Conversion(value);
   isAuthenticate = (value == motDePasseAttendu);
